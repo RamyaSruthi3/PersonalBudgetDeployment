75158657dcf86de04c51745a18cdf0c83d0b4e80
Ramya <rreddy11@uncc.edu>
2023-12-11 21:44:34 -0500

    Final Project Deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "http://167.172.30.174:5000/api/v1";
 axios.defaults.withCredentials = true;
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
